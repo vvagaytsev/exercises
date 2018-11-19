8bce07b865c8ecb91a796e73213ae228e2b5359c
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2018-11-19 23:32:05 +0200

    Longest substring with unique characters: impl corrections and test

--- src/main/java/other/LongestSubstringWithUniqueCharacters.java
+++ src/main/java/other/LongestSubstringWithUniqueCharacters.java
@@ -1,22 +1,16 @@
 package other;
 
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author Vladimir Vagaytsev
  * Date: 19/11/2018
  */
 public class LongestSubstringWithUniqueCharacters {
-
-    public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("pwwkkeerrf"));
-        System.out.println(lengthOfLongestSubstring("abcabcbb"));
-        System.out.println(lengthOfLongestSubstring("bbbbb"));
-        System.out.println(lengthOfLongestSubstring("pwwkew"));
-        System.out.println(lengthOfLongestSubstring("pwwddkewa"));
-        System.out.println(lengthOfLongestSubstring("dvdf"));
-    }
 
     public static int lengthOfLongestSubstring(String s) {
         if (s.isEmpty()) {
@@ -30,7 +24,11 @@ public class LongestSubstringWithUniqueCharacters {
                 if (usedChars.size() > lastKnownResult) {
                     lastKnownResult = usedChars.size();
                 }
-                usedChars.clear();
+                Iterator<Character> iterator = usedChars.iterator();
+                while (iterator.hasNext() && iterator.next() != c) {
+                    iterator.remove();
+                }
+                usedChars.remove(c);
                 usedChars.add(c);
             }
         }
@@ -38,5 +36,45 @@ public class LongestSubstringWithUniqueCharacters {
             lastKnownResult = usedChars.size();
         }
         return lastKnownResult;
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(2, lengthOfLongestSubstring("pwwkkeerrf"));
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(1, lengthOfLongestSubstring("bbbbb"));
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertEquals(3, lengthOfLongestSubstring("pwwkew"));
+    }
+
+    @Test
+    public void test4() {
+        Assert.assertEquals(5, lengthOfLongestSubstring("pwwddkewa"));
+    }
+
+    @Test
+    public void test5() {
+        Assert.assertEquals(3, lengthOfLongestSubstring("dvdf"));
+    }
+
+    @Test
+    public void test6() {
+        Assert.assertEquals(4, lengthOfLongestSubstring("davdf"));
+    }
+
+    @Test
+    public void test7() {
+        Assert.assertEquals(5, lengthOfLongestSubstring("dagvdf"));
+    }
+
+    @Test
+    public void test8() {
+        Assert.assertEquals(6, lengthOfLongestSubstring("ohvhjdml"));
     }
 }
